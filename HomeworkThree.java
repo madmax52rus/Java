dc0c4c9954b5b405a1306b9b5496f265b98f83f0
madmax52rus <88784403+madmax52rus@users.noreply.github.com>
2022-02-25 08:43:32 +0300

    Update HomeworkThree.java

--- HomeworkThree.java
+++ HomeworkThree.java
@@ -2,7 +2,7 @@
 * Geekbrains. Java 1. Homework #3
 *
 * @author Ryabov Maxim
-* @version 24.02.2022
+* @version 25.02.2022
 */
 
 import java.util.Arrays;
@@ -11,7 +11,7 @@ import java.util.Random;
 class HomeworkThree {
     public static void main(String[] args) {
     
-    System.out.println("Item 1");
+        System.out.println("Item 1");
         
         int[] arrInvert = { 1, 0, 1, 0, 1, 0};
         System.out.println(Arrays.toString(arrInvert));
@@ -22,7 +22,7 @@ class HomeworkThree {
         System.out.println(Arrays.toString(arrInvert));        
        
        
-    System.out.println("Item 2");
+        System.out.println("Item 2");
        
         int[] arrFilled = new int[100];
         for (int i = 0; i < arrFilled.length; i++) {
@@ -31,7 +31,7 @@ class HomeworkThree {
         System.out.println(Arrays.toString(arrFilled));     
     
     
-    System.out.println("Item 3");
+        System.out.println("Item 3");
     
         int[] arrChange = {1, 5, 3, 2, 11, 4, 5, 2, 4, 8, 9, 1 };
         for (int i = 0; i < arrChange.length; i++) {
@@ -42,7 +42,7 @@ class HomeworkThree {
         System.out.println(Arrays.toString(arrChange));      
 
 
-    System.out.println("Item 4");        
+        System.out.println("Item 4");        
 
         int[][] arrDiagonal = new int[6][6];
         for (int i = 0; i < 6; i++) {
@@ -50,15 +50,13 @@ class HomeworkThree {
             arrDiagonal[i][arrDiagonal.length -i -1] = 1;
             System.out.println(Arrays.toString(arrDiagonal[i]));
         }
-      
-    
-    System.out.println("Item 5");    
-        int len = 4, initialValue = 6;   
-        int arr[] = new int[len];
-        for (int i = 0; i < len; i++) {
-            arr[i] = initialValue;
-        }
-        System.out.print(Arrays.toString(arr));
+        
+       // System.out.println("Item 5");    
     }   
+        
+    static int[] arrCreate(int len, int initialValue) {
+        int arr[] = new int[len];
+        Arrays.fill(arr, initialValue);
+        return arr;
+    }
 }    
-   
